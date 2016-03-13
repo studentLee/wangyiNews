431ae63bf84517c7f0c8b6cb06d5233f0a1a5861
lilei <505130980@qq.com>
2016-03-13 19:27:54 +0800

    初始化

---
+++ 网易新闻--01/网易新闻--01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易新闻--01
+//
+//  Created by 李磊 on 16/3/13.
+//  Copyright © 2016年 lilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
